200c8a8f1598087c96476744d9b12e6b2d47d16c
Filipe Versehgi <filipe.silva@tegra.me>
2018-08-21 16:33:10 -0300

    Estrutura inicial criada

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,29 +1,81 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <router-link :to="item.route">
+            <v-list-tile-action>
+              <v-icon v-html="item.icon"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            </v-list-tile-content>
+          </router-link>
+        </v-list-tile>
+
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+    >
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+
+    </v-toolbar>
+    <v-content>
+      
+      <router-view/>
+      
+    </v-content>
+   
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { IMenuItem } from './interfaces/menu';
+
+@Component
+export default class App extends Vue {
+  private clipped: boolean = true;
+  private items: IMenuItem[] = [];
+  private miniVariant: boolean = false;
+  private title: string = 'Vue Seed';
+
+  private beforeMount() {
+    // Inicializar o Menu
+    this.items = [{
+      icon: 'dashboard',
+      title: 'Dashboard',
+      route: '/',
+      role: ''
+    },
+    {
+      icon: 'information',
+      title: 'About',
+      route: '/about',
+      role: ''
+    }];
   }
 }
-</style>
+</script>
---
+++ Vue/src/interfaces/menu.ts
@@ -0,0 +1,6 @@
+export interface IMenuItem {
+    route: string;
+    title: string;
+    icon: string;
+    role: string;
+}
